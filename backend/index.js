e28f83184ec862163341c8404625e7169bc61dc5
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-03-10 10:55:56 +0530

    created the backend and front end need to find a free ai gen api to use

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+
+// 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
+
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
+
+const startServer = async () => {
+  try {
+    // database is connected with tharindu.20221838@iit.ac.lk mongodb atlas account  
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
